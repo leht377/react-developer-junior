957c3ac4b3b1e7fd7daa68478454f35ea59a139d
Luis Eduardo Tenorio <Migithub2019@gmail.com>
2025-03-06 23:04:25 -0500

    Enhance getEmployees function to support pagination and return paginated results

--- src/services/employee.service.ts
+++ src/services/employee.service.ts
@@ -1,11 +1,19 @@
-import { EmployeeEntity } from "@/domain/entities/employe.entity"
-import {employeesList } from "../data/employee"
-import { EmployeMapper } from "@/utils/mappers/employe.mapper"
+import { EmployeeEntity } from '@/domain/entities/employe.entity'
+import { employeesList } from '../data/employee'
+import { EmployeMapper } from '@/utils/mappers/employe.mapper'
+import { generatePagination } from '@/utils/helpers/generatePagination.helper'
+import { Pagination } from '@/domain/types/pagination'
 
-const getEmployees = (): EmployeeEntity[] => {
+const getEmployees = (
+  page: number = 330,
+  limit: number = 10
+): { data: EmployeeEntity[]; pagination: Pagination } => {
   const data = employeesList.data.slice(0, 10)
   const dataMapped = data.map((d) => EmployeMapper(d))
-  return dataMapped
+
+  const pagination = generatePagination(employeesList.data, page, limit)
+
+  return { data: dataMapped, pagination }
 }
 
 export { getEmployees }
