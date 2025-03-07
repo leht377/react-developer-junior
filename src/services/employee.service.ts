6f33c16eb380dd33510015ce0b42be99d15a7243
Luis Eduardo Tenorio <Migithub2019@gmail.com>
2025-03-06 22:08:27 -0500

    Rename getEmployee function to getEmployees for clarity and consistency

--- src/services/employee.service.ts
+++ src/services/employee.service.ts
@@ -2,10 +2,10 @@ import { EmployeeEntity } from "@/domain/entities/employe.entity"
 import {employeesList } from "../data/employee"
 import { EmployeMapper } from "@/utils/mappers/employe.mapper"
 
-const getEmployee = (): EmployeeEntity[] => {
+const getEmployees = (): EmployeeEntity[] => {
   const data = employeesList.data.slice(0, 10)
   const dataMapped = data.map((d) => EmployeMapper(d))
   return dataMapped
 }
 
-export { getEmployee }
+export { getEmployees }
