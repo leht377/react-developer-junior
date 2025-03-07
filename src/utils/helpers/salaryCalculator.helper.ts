2928af8748022266544dacfc4d8247fbfccef4fc
Luis Eduardo Tenorio <Migithub2019@gmail.com>
2025-03-07 17:38:02 -0500

    Implement minimum rate checks for various hourly calculations in salaryCalculator.helper.ts

--- src/utils/helpers/salaryCalculator.helper.ts
+++ src/utils/helpers/salaryCalculator.helper.ts
@@ -1,6 +1,13 @@
 import {
   DAYTIME_OVERTIME_HOUR_PERCENTAGE,
   HOLIDAY_SHIFT_SURCHARGE_PERCENTAGE,
+  MIN_DAYTIME_HOLIDAY_OVERTIME_HOUR_RATE,
+  MIN_DAYTIME_OVERTIME_HOUR_RATE,
+  MIN_HOLIDAY_SHIFT_RATE,
+  MIN_NIGHT_HOLIDAY_SHIFT_RATE,
+  MIN_NIGHT_SHIFT_RATE,
+  MIN_NIGHTTIME_HOLIDAY_OVERTIME_HOUR_RATE,
+  MIN_NIGHTTIME_OVERTIME_HOUR_RATE,
   NIGHT_SHIFT_SURCHARGE_PERCENTAGE,
   NIGHTTIME_OVERTIME_HOUR_PERCENTAGE
 } from '@/constant'
@@ -10,7 +17,8 @@ import {
  * @returns {number} Valor de la hora con el recargo nocturno.
  */
 const calculateNightShiftHourlyRate = (hourlyRate: number): number => {
-  return hourlyRate + hourlyRate * NIGHT_SHIFT_SURCHARGE_PERCENTAGE
+  const calculatedValue = hourlyRate + hourlyRate * NIGHT_SHIFT_SURCHARGE_PERCENTAGE
+  return calculatedValue < MIN_NIGHT_SHIFT_RATE ? MIN_NIGHT_SHIFT_RATE : calculatedValue
 }
 
 /**
@@ -18,7 +26,8 @@ const calculateNightShiftHourlyRate = (hourlyRate: number): number => {
  * @returns {number} Valor de la hora con el recargo por festivo o domingo.
  */
 const calculateHolidayShiftHourlyRate = (hourlyRate: number): number => {
-  return hourlyRate + hourlyRate * HOLIDAY_SHIFT_SURCHARGE_PERCENTAGE
+  const calculatedValue = hourlyRate + hourlyRate * HOLIDAY_SHIFT_SURCHARGE_PERCENTAGE
+  return calculatedValue < MIN_HOLIDAY_SHIFT_RATE ? MIN_HOLIDAY_SHIFT_RATE : calculatedValue
 }
 
 /**
@@ -26,10 +35,12 @@ const calculateHolidayShiftHourlyRate = (hourlyRate: number): number => {
  * @returns {number} Valor de la hora con ambos recargos aplicados.
  */
 const calculateHolidayNightShiftHourlyRate = (hourlyRate: number): number => {
-  return (
+  const calculatedValue =
     hourlyRate +
     hourlyRate * (NIGHT_SHIFT_SURCHARGE_PERCENTAGE + HOLIDAY_SHIFT_SURCHARGE_PERCENTAGE)
-  )
+  return calculatedValue < MIN_NIGHT_HOLIDAY_SHIFT_RATE
+    ? MIN_NIGHT_HOLIDAY_SHIFT_RATE
+    : calculatedValue
 }
 
 /**
@@ -37,7 +48,10 @@ const calculateHolidayNightShiftHourlyRate = (hourlyRate: number): number => {
  * @returns {number} Valor de la hora extra diurna.
  */
 const calculateHourlyDaytimeOvertimeRate = (hourlyRate: number): number => {
-  return hourlyRate + hourlyRate * DAYTIME_OVERTIME_HOUR_PERCENTAGE
+  const calculatedValue = hourlyRate + hourlyRate * DAYTIME_OVERTIME_HOUR_PERCENTAGE
+  return calculatedValue < MIN_DAYTIME_OVERTIME_HOUR_RATE
+    ? MIN_DAYTIME_OVERTIME_HOUR_RATE
+    : calculatedValue
 }
 
 /**
@@ -45,7 +59,10 @@ const calculateHourlyDaytimeOvertimeRate = (hourlyRate: number): number => {
  * @returns {number} Valor de la hora extra nocturna.
  */
 const calculateHourlyNighttimeOvertimeRate = (hourlyRate: number): number => {
-  return hourlyRate + hourlyRate * NIGHTTIME_OVERTIME_HOUR_PERCENTAGE
+  const calculatedValue = hourlyRate + hourlyRate * NIGHTTIME_OVERTIME_HOUR_PERCENTAGE
+  return calculatedValue < MIN_NIGHTTIME_OVERTIME_HOUR_RATE
+    ? MIN_NIGHTTIME_OVERTIME_HOUR_RATE
+    : calculatedValue
 }
 
 /**
@@ -53,10 +70,12 @@ const calculateHourlyNighttimeOvertimeRate = (hourlyRate: number): number => {
  * @returns {number} Valor de la hora extra diurna en festivo o domingo.
  */
 const calculateDaytimeHolidayOvertimeRate = (hourlyRate: number): number => {
-  return (
+  const calculatedValue =
     hourlyRate +
     hourlyRate * (DAYTIME_OVERTIME_HOUR_PERCENTAGE + HOLIDAY_SHIFT_SURCHARGE_PERCENTAGE)
-  )
+  return calculatedValue < MIN_DAYTIME_HOLIDAY_OVERTIME_HOUR_RATE
+    ? MIN_DAYTIME_HOLIDAY_OVERTIME_HOUR_RATE
+    : calculatedValue
 }
 
 /**
@@ -64,10 +83,12 @@ const calculateDaytimeHolidayOvertimeRate = (hourlyRate: number): number => {
  * @returns {number} Valor de la hora extra nocturna en festivo o domingo.
  */
 const calculateNighttimeHolidayOvertimeRate = (hourlyRate: number): number => {
-  return (
+  const calculatedValue =
     hourlyRate +
     hourlyRate * (NIGHTTIME_OVERTIME_HOUR_PERCENTAGE + HOLIDAY_SHIFT_SURCHARGE_PERCENTAGE)
-  )
+  return calculatedValue < MIN_NIGHTTIME_HOLIDAY_OVERTIME_HOUR_RATE
+    ? MIN_NIGHTTIME_HOLIDAY_OVERTIME_HOUR_RATE
+    : calculatedValue
 }
 
 export const salaryCalculator = {
