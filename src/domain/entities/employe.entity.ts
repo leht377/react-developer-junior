2bb4df1856cb78f61b784f886b02a347de0e392b
Luis Eduardo Tenorio <Migithub2019@gmail.com>
2025-03-07 17:11:07 -0500

    Add hourly rate calculation to EmployeeEntity

--- src/domain/entities/employe.entity.ts
+++ src/domain/entities/employe.entity.ts
@@ -1,8 +1,11 @@
+import { DAYS_OF_MONTH, WORKING_DAYS_PER_WEEK } from '@/constant'
 import { AccessControls } from '@/domain/types/accessControl'
 import { EmployeeAttributes } from '@/domain/types/employee'
 import { Workshifts } from '@/domain/types/workshifts'
 
 export class EmployeeEntity {
+  public readonly hourlyRate: number
+
   constructor(
     public readonly type: string,
     public readonly id: number,
@@ -11,5 +14,20 @@ export class EmployeeEntity {
       workshifts: Workshifts[]
       accessControls: AccessControls[]
     }
-  ) {}
+  ) {
+    this.hourlyRate = this.calculateRegularHourlyRate()
+  }
+
+  private calculateRegularHourlyRate = (): number => {
+    const maxWeeklyHours = this.relationships.workshifts[0]?.attributes?.maximun_weekly_hours ?? 0
+    const dailyWorkingHours = maxWeeklyHours / WORKING_DAYS_PER_WEEK
+
+    if (dailyWorkingHours <= 0) return 0 // Prevent division by 0
+
+    const monthlyWorkedHours = dailyWorkingHours * DAYS_OF_MONTH
+
+    if (monthlyWorkedHours <= 0) return 0 // Prevent division by 0
+
+    return this.attributes.salary / monthlyWorkedHours
+  }
 }
