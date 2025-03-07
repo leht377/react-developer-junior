87803a565da8b75eb6babc5ccf40d822c96203d8
Luis Eduardo Tenorio <Migithub2019@gmail.com>
2025-03-06 23:04:01 -0500

    Add Pagination type definition for pagination functionality

---
+++ src/domain/types/pagination.ts
@@ -0,0 +1,10 @@
+export type Pagination = {
+  total: number;
+  limit: number;
+  totalPages: number;
+  page: number;
+  hasPrevPage: boolean;
+  hasNextPage: boolean;
+  prevPage: number | null;
+  nextPage: number | null;
+};
